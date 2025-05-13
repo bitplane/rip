f7aeb034196ee38de061b96df305b2b449618543
Gareth Davidson <gaz@bitplane.net>
2025-05-13 16:37:15 +0100

    ui

--- scrip/lib_ui.sh
+++ scrip/lib_ui.sh
@@ -43,3 +43,38 @@ ui_emoji() {
   fi
 }
 
+# Clears a widget's buffer
+
+
+
+# Adds a widget to the ui graph
+# Usage: ui_widget_add type name x y w h
+ui_widget_add() {
+    local t=$1 parent="$7" path="$7/$2" x="$3" y="$4" w="$5" h="$6"
+    mkdir -p "$path"
+    echo "$t"               | meta_set "ui.type"   0 "$path"
+    echo "$parent"          | meta_set "ui.parent" 0 "$path"
+    printf "$x\n$y\n"       | meta_set "ui.pos"    0 "$path"
+    printf "$w\n$h\n"       | meta_set "ui.size"   0 "$path"
+    printf "0\n0\n$w\n$h\n" | meta_set "ui.clip"   0 "$path"
+    ui_widget_buffer_new "$w" "$h" "$path"
+}
+
+# Draw this widget
+# Usage: ui_widget_draw path
+ui_widgeit_draw() {
+    t < <(meta_get ui.type 0 $1)
+    "ui_widget_draw_$t" $1
+    for child in ${1}/*; do
+        ui_widget_draw $child
+    done
+}
+
+ui_widget_draw_screen() {
+    read w h < <(meta_get ui.pos 0 "$1")
+    ui_center_text $w "$(meta_get ui.title 0 "$1")" | meta_set "ui.buffer" 0 "$1"
+}
+
+
+
+
