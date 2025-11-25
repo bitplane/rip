da8eb7a623cb6b055274077f4fb61af77a674077
Gareth Davidson <gaz@bitplane.net>
2025-11-25 12:11:00 +0000

    POSIX awk compatability

--- scrip/lib_meta_hooks.sh
+++ scrip/lib_meta_hooks.sh
@@ -8,8 +8,17 @@
 meta_hook_ddrescue.log() {
     (
     awk '
+    function hex2dec(h,  n, i, c, v) {
+        h = tolower(h); sub(/^0x/, "", h); n = 0
+        for (i = 1; i <= length(h); i++) {
+            c = substr(h, i, 1)
+            v = index("0123456789abcdef", c) - 1
+            n = n * 16 + v
+        }
+        return n
+    }
     /^0x/ {
-        size   =  strtonum($2)
+        size   = hex2dec($2)
         total  += size
         if ($3 == "+") good += size
     }
